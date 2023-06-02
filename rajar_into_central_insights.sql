7a1bc268bae03b2c3bab8d5adbce5456ff526c49
Aron Farmer <58588282+AronFarmer@users.noreply.github.com>
2023-06-02 13:06:39 +0100

    Update rajar_into_central_insights.sql

--- rajar_into_central_insights.sql
+++ rajar_into_central_insights.sql
@@ -1,20 +1,43 @@
-delete from central_insights_sandbox.af_rajar_table_1 where quarter in (select distinct quarter from rajar.summary_table_1);
-insert into central_insights_sandbox.af_rajar_table_1 select * from rajar.summary_table_1;
+-------------------------------------
+--- RUN THIS ON RAJAR DAY ---
+-------------------------------------
+
+delete
+from central_insights_sandbox.af_rajar_table_1
+where quarter in (select distinct quarter from rajar.summary_table_1);
+insert into central_insights_sandbox.af_rajar_table_1
+select * from rajar.summary_table_1
+;
 grant all on central_insights_sandbox.af_rajar_table_1 to group rajar_users;
 grant all on central_insights_sandbox.af_rajar_table_1 to jasmine_breeze;
 grant all on central_insights_sandbox.af_rajar_table_1 to samuel_sanyaolu;
 grant all on central_insights_sandbox.af_rajar_table_1 to jonathan_roussot;
+grant all on central_insights_sandbox.af_rajar_table_1 to matthew_byrne;
 
-delete from central_insights_sandbox.af_rajar_table_2 where quarter in (select distinct summary_table_2.quarter from rajar.summary_table_2);
-insert into central_insights_sandbox.af_rajar_table_2 select * from rajar.summary_table_2;
+
+
+delete
+from central_insights_sandbox.af_rajar_table_2
+where quarter in (select distinct quarter from rajar.summary_table_2);
+insert into central_insights_sandbox.af_rajar_table_2
+select *
+                                                           from rajar.summary_table_2
+;
 grant all on central_insights_sandbox.af_rajar_table_2 to group rajar_users;
 grant all on central_insights_sandbox.af_rajar_table_2 to jasmine_breeze;
 grant all on central_insights_sandbox.af_rajar_table_2 to samuel_sanyaolu;
 grant all on central_insights_sandbox.af_rajar_table_2 to jonathan_roussot;
+grant all on central_insights_sandbox.af_rajar_table_2 to matthew_byrne;
 
-delete from central_insights_sandbox.rajar_population where file_quarter in (select distinct file_quarter from rajar.rajar_population);
-insert into central_insights_sandbox.rajar_population select * from rajar.rajar_population;
+delete
+from central_insights_sandbox.rajar_population
+where file_quarter in (select distinct file_quarter from rajar.rajar_population);
+insert into central_insights_sandbox.rajar_population
+select * from rajar.rajar_population
+;
 grant all on central_insights_sandbox.rajar_population to group rajar_users;
 grant all on central_insights_sandbox.rajar_population to jasmine_breeze;
 grant all on central_insights_sandbox.rajar_population to samuel_sanyaolu;
 grant all on central_insights_sandbox.rajar_population to jonathan_roussot;
+grant all on central_insights_sandbox.rajar_population to matthew_byrne;
+;
